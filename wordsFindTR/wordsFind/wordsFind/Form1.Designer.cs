e54ff646658279e03a6353fe396ebb06d865bca2
Albedo426 <65228887+Albedo426@users.noreply.github.com>
2020-05-12 15:38:22 +0300

    Add files via upload

---
+++ wordsFindTR/wordsFind/wordsFind/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace wordsFind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lettertxt = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.selectedword = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Puanlb = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nextbuttton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lettertxt
+            // 
+            this.lettertxt.Enabled = false;
+            this.lettertxt.Location = new System.Drawing.Point(6, 44);
+            this.lettertxt.Name = "lettertxt";
+            this.lettertxt.Size = new System.Drawing.Size(125, 22);
+            this.lettertxt.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nextbuttton);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.Puanlb);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.selectedword);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lettertxt);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 266);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kelime Oyunu";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(137, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "en uzun kelime";
+            // 
+            // selectedword
+            // 
+            this.selectedword.Enabled = false;
+            this.selectedword.Location = new System.Drawing.Point(244, 202);
+            this.selectedword.Name = "selectedword";
+            this.selectedword.Size = new System.Drawing.Size(100, 22);
+            this.selectedword.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 27);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Yeni Oyun";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(134, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "bulunan kelimeler";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(137, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(207, 148);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 27);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Başla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "boşluk bırakın";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Her harften sonra ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "kendiniz seçin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Seçilen kelimeler";
+            // 
+            // Puanlb
+            // 
+            this.Puanlb.AutoSize = true;
+            this.Puanlb.Location = new System.Drawing.Point(205, 233);
+            this.Puanlb.Name = "Puanlb";
+            this.Puanlb.Size = new System.Drawing.Size(16, 17);
+            this.Puanlb.TabIndex = 10;
+            this.Puanlb.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(137, 233);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Puanınız :";
+            // 
+            // nextbuttton
+            // 
+            this.nextbuttton.Location = new System.Drawing.Point(6, 165);
+            this.nextbuttton.Name = "nextbuttton";
+            this.nextbuttton.Size = new System.Drawing.Size(125, 27);
+            this.nextbuttton.TabIndex = 12;
+            this.nextbuttton.Text = "Sıradaki Harfler";
+            this.nextbuttton.UseVisualStyleBackColor = true;
+            this.nextbuttton.Click += new System.EventHandler(this.nextbuttton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 343);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lettertxt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox selectedword;
+        private System.Windows.Forms.Button nextbuttton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label Puanlb;
+    }
+}
+
---
+++ wordsFindTR/wordsFind/wordsFind/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wordsFind
+{
+    public partial class Form1 : Form
+    {
+        String[,] dizi=new String[,] { 
+                                    { "a", "0" },{ "b", "0" },{ "c", "0" },{ "ç", "0" },{ "d", "0"},{ "e","0" },
+                                    { "f", "0" },{ "g", "0" },{ "ğ", "0" },{ "h", "0" },{ "ı", "0" },{ "i", "0" },
+                                    { "j", "0" },{ "k", "0" },{ "l", "0" },{ "m", "0" },{ "n", "0" },{ "o", "0" },
+                                    { "ö", "0" },{ "p", "0" },{ "r", "0" },{ "s", "0" },{ "ş", "0" },{ "t","0" },
+                                    { "u", "0" },{ "ü", "0" },{ "v", "0" },{ "y", "0" },{ "z", "0" }
+        };
+        private int puan = 0;
+        String[,] character = new String[8, 2];
+        public void setup() {
+            int item = 0;
+            lettertxt.Text = "";
+            selectedword.Text = "";
+            Random rand = new Random();
+            for (int i = 0; i < 8; i++)
+            {
+                item = rand.Next(0, 29);
+                lettertxt.Text += dizi[item, 0]+ " ";
+            }
+            
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            setup();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filePath = Application.StartupPath + "\\TDK_Sözlük_Kelime_Listesi.txt";
+            button2.Enabled = false;
+            int k = 0;
+            string[] words = lettertxt.Text.Split(' ');
+            foreach (var item in words)
+            {
+                for (int i = 0; i < 29; i++)
+                {
+                    if (item == dizi[i, 0])
+                    {
+                        dizi[i, 1] = (Convert.ToInt16(dizi[i, 1]) + 1).ToString(); ;
+                    }
+                }
+            }
+
+            for (int i = 0; i < (dizi.Length / 2) - 1; i++)
+            {
+
+                if (Convert.ToInt16(dizi[i, 1]) != 0)
+                {
+                    character[k, 0] = dizi[i, 0];
+                    character[k, 1] = dizi[i, 1];
+                    k++;
+                }
+
+            }
+            String word = "";//kelimemiz
+            int mincarakter = 4;//minimum karakter sayısı kelimede min kaç karakter olsun
+            int control = 0;//control bulunan kelimeyle listedeki kelimenin  harrf sayısı aynı mı koonrol 
+            int coni = 1;//joker hakkı
+            int temp = 0;//soraki kelimeye geçtiğimde jokeri kullnamam için
+            var kelimeler = File.ReadLines(filePath);
+            String[,] tempcharacter = new String[8, 2];
+            Array.Copy(character, tempcharacter, character.Length);
+            List<String> selecword = new List<String>();//bulunan kelimeleri tuutğum adres
+            foreach (var kelimes in kelimeler)
+            {
+
+
+                foreach (var achar in kelimes)
+                {
+                    temp = 0;
+                    for (int i = 0; i < (character.Length / 2) - 1; i++)
+                    {
+                        if (character[i, 0]==achar.ToString() && Convert.ToInt16(character[i, 1]) > 0)
+                        {
+                            control++;
+                            character[i, 1] = (Convert.ToInt16(character[i, 1]) - 1).ToString();
+                            word += character[i, 0];
+                            temp++;
+                        }
+                    }
+
+                    if (0 == temp && coni == 1)
+                    {
+                        coni--;
+                        control++;
+                        word += "("+ achar+")";
+                    }
+                    if (coni == 1)
+                    {
+                        temp = 0;
+                    }
+                }
+                if (control== kelimes.Length && control>= mincarakter) {
+                    selecword.Add(word);
+                }
+                Array.Copy(tempcharacter, character, tempcharacter.Length);
+                temp = 0;
+                word = "";
+                control = 0;
+                coni = 1;
+            }
+            listBox1.Items.AddRange(selecword.ToArray());
+            selectedword.Text = selectWord(selecword);
+            lettertxt.Enabled = false;
+
+        }
+        public int score(String maxAge) {
+            maxAge = maxAge.Replace(")", "");
+            maxAge = maxAge.Replace("(", "");
+            int count = maxAge.Length;
+            if (count == 3)
+            {
+                puan += 3;
+            }
+            else if (count == 4)
+            {
+                puan += 4;
+            }
+            else if (count == 5)
+            {
+                puan += 5;
+            }
+            else if (count == 6)
+            {
+                puan += 7;
+            }
+            else if (count == 7)
+            {
+                puan += 9;
+            }
+            else if (count == 8)
+            {
+                puan += 11;
+            }
+            else {
+                puan += 15;
+            }
+            return puan;
+        }
+        public String selectWord(List<String> words) {
+            String maxAge = words.ElementAt(0);
+            foreach (String word in words)
+            {
+                if (maxAge.Length < word.Length)
+                {
+                    maxAge = word;
+                }
+            }
+            Puanlb.Text=score(maxAge).ToString();
+            return maxAge;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lettertxt.Enabled = true;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lettertxt.Enabled = false;
+            button2.Enabled = true;
+            listBox1.Items.Clear();
+            for (int i = 0; i < (dizi.Length / 2) - 1; i++)
+            {
+                    dizi[i, 1] = "0";
+            }
+            setup();
+            puan = 0;
+            Puanlb.Text = "0";
+        }
+        private void nextbuttton_Click(object sender, EventArgs e)
+        {
+            lettertxt.Enabled = false;
+            button2.Enabled = true;
+            for (int i = 0; i < (dizi.Length / 2) - 1; i++)
+            {
+                dizi[i, 1] = "0";
+            }
+            setup();
+            Puanlb.Text = puan.ToString();
+        }
+    }
+}
